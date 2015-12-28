fa5874029e3438893d5210ca179d142d1e2f98f7
Hugo Posca <hposca@users.noreply.github.com>
2015-12-28 11:24:48 -0200

    Updating the firefox download URL
    As stated on
    http://ftp.mozilla.org/pub/firefox/releases/latest/README.txt

--- update_firefox.sh
+++ update_firefox.sh
@@ -14,12 +14,13 @@ set -u
 #########
 
 _temp_dir=$(mktemp -d)
+echo "$(color lightyellow ">>>>> Created temporary directory: $_temp_dir")"
 cd $_temp_dir
 
 echo "$(color green ">>>>> Downloading newest version of firefox...")"
 
-_firefox_url="http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-x86_64/en-US/"
-wget -A.bz2 -r -np -nd $_firefox_url -P .
+_firefox_url="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+wget -O firefox.tar.bz2 $_firefox_url
 
 echo "$(color green ">>>>> Extracting files...")"
 
