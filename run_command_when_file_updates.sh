52b100b4365db7124d269dabb7196b2b0f58099f
Hugo Posca <hugo@rog>
2018-07-21 16:59:48 -0700

    Import correctly regardless of execution location

--- run_command_when_file_updates.sh
+++ run_command_when_file_updates.sh
@@ -5,8 +5,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 set +u
-source shml.sh
+source "${SCRIPT_DIR}/shml.sh"
 set -u
 #########
 # Begin #
