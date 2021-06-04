ae664cfa349350f9865dbd41352d7d8ca120df60
Hugo Posca <hugo@gruntwork.io>
2021-06-04 11:47:55 -0700

    Update fetch_all script
    Added logic to behave like `git pull` if we are on a master-like branch

--- fetch_all.sh
+++ fetch_all.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 
-# Do a `git fetch` on all repositories recursively,
-# starting from the current directory.
+# Script used to "git pull" into all repositories, beginning from the root directory.
+# It actually fetches first, then if not on a feature branch, reset to origin.
 
-_command="git fetch --all"
-_parent_dir=$(pwd)
+function fetch {
+  # git fetch --prune --all
+  git fetch --all
+}
 
-find . -type d -name ".git" | while read _directory; do
-  _dir_with_repo=${_directory%.*} # Removes the `.git` at the end
-  cd $_dir_with_repo
-  echo "Inside $_dir_with_repo ..."
+function git_current_branch {
+  git rev-parse --abbrev-ref HEAD
+}
 
-  $_command
+function is_master {
+  local branch="${1}"
+  local master_branches="master main trunk base"
 
+  echo "${master_branches}" | grep -w "${branch}" > /dev/null
+}
+
+find . -maxdepth 2 -type d -name ".git" | while read _directory; do
+  _dir_with_repo=${_directory%.*} # Removes the '.git' at the end
+  pushd "${_dir_with_repo}"
+  echo "Inside ${_dir_with_repo} ..."
+
+  fetch
+  if is_master $(git_current_branch); then
+    git reset origin/$(git_current_branch) --hard
+  fi
+
+  popd
   echo "---"
-  cd $_parent_dir
 done
